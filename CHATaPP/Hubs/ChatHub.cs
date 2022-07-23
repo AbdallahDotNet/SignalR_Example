b4e6df3c447fb94c001ba0ed413c9f463b7592f0
dell <119abadallah118@gmail.com>
2022-07-23 14:29:02 +0200

    First commit

---
+++ CHATaPP/Hubs/ChatHub.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CHATaPP.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string message)
+        {
+            Clients.All.SendAsync("receiveMessage", message);
+            Clients.Caller.SendAsync("receiveMessage", "your message");
+            Clients.Others.SendAsync("receiveMessage", "not your message");
+        }
+
+        public void SendMessageToOneUser(string connectionId, string message)
+        {
+            Clients.Client(connectionId).SendAsync("receiveMessage", message);
+            Clients.Caller.SendAsync("receiveMessage", message);
+        }
+
+        public void SendGroupMessage(string groupName, string message)
+        {
+            Clients.Group(groupName).SendAsync("receiveMessage", message);
+        }
+
+        public void JoinGroup(string groupName)
+        {
+            Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            Clients.Group(groupName).SendAsync("receiveMessage", $"{Context.ConnectionId} joined");
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Clients.All.SendAsync("receiveMessage", $"{Context.ConnectionId} : connected");
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Clients.All.SendAsync("receiveMessage", $"{Context.ConnectionId} : Disconnected");
+            return base.OnDisconnectedAsync(exception);
+        }
+
+    }
+}
---
+++ CHATaPP/wwwroot/js/site.js
@@ -0,0 +1,40 @@
+﻿
+    const connection = new signalR.HubConnectionBuilder()
+        .withUrl("https://localhost:44368/chat")
+        .configureLogging(signalR.LogLevel.Debug)
+        .build();
+
+    connection.on("receiveMessage", (message) => {
+        const msg = message.replace(/&/g, "&").replace(/</g, "<").replace(/>/g, ">");
+        const encodedMsg = msg;
+        const li = document.createElement("li");
+        li.textContent = encodedMsg;
+        document.getElementById("messagesList").appendChild(li);
+    });
+
+
+    connection.start().catch(err => console.error(err.toString()));
+
+    $("#send").on("click", function () {
+        const message = document.getElementById("message").value;
+        const groupName = document.getElementById("groupName").value;
+        const connectionId = document.getElementById("conId").value;
+
+        if (groupName !== "") {
+            connection.invoke("SendGroupMessage", groupName, message).catch(err => console.error(err.toString()));
+        } else if (connectionId !== "") {
+            const message = document.getElementById("message").value;
+            connection.invoke("SendMessageToOneUser", connectionId, message).catch(err => console.error(err.toString()));
+        }
+        else {
+            connection.invoke("SendMessage", message).catch(err => console.error(err.toString()));
+        }
+
+        event.preventDefault();
+    })
+
+    $("#join").on("click", function () {
+        const groupName = document.getElementById("groupName").value;
+        connection.invoke("JoinGroup", groupName).catch(err => console.error(err.toString()));
+        event.preventDefault();
+    })
